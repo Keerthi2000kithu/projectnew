97525d90a2b4c2cdc4f22a901b67c3da2d0ec729
Keerthi2000kithu <keerthip194@gmail.com>
2024-02-21 20:01:12 +0530

    search student created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Addstudents from './Components/Addstudents';
+import Searchstudent from './Components/Searchstudent';
 
 function App() {
   return (
     <div >
      <Addstudents/>
+     <Searchstudent/>
     </div>
   );
 }
---
+++ src/Components/Searchstudent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Searchstudent = () => {
+  return (
+    <div>
+        <label htmlFor="" className="form-label">Name</label>
+        <input type="text" className="form-control" />
+        <button className="btn btn-success"></button>
+    </div>
+
+  )
+}
+
+export default Searchstudent
