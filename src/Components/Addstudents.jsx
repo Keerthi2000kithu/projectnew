164346ba1261f540c113053fdbb221e97e0f9d33
Keerthi2000kithu <keerthip194@gmail.com>
2024-02-21 19:13:53 +0530

    addstudent branch

---
+++ src/Components/Addstudents.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Addstudents = () => {
+  return (
+    <div>
+        <label htmlFor="" className="form-label">Name</label>
+        <input type="text" className="form-control" />
+        <button className="btn">Add</button>btn-success
+    </div>
+  )
+}
+
+export default Addstudents
